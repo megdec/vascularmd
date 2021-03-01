5191a7994d5f89c5e02eff1c101f37640b3f6a01
Meghane Decroocq <decroocq@lilidell.creatis.insa-lyon.fr>
2021-03-01 14:42:18 +0100

    Import result fields

--- Simulation.py
+++ Simulation.py
@@ -25,6 +25,23 @@ class Simulation:
 
 		self.arterial_tree = arterial_tree
 		self.output_dir = output_dir
+		
+
+		crsec_graph = self.arterial_tree.get_crsec_graph()
+
+		self.inlet_nodes = []
+		self.outlet_nodes = []
+
+		for n in crsec_graph.nodes():
+			if crsec_graph.nodes[n]['type'] == "end":
+				if crsec_graph.in_degree(n) == 0: # Inlet case
+					self.inlet_nodes.append(n)
+				else:
+					self.outlet_nodes.append(n)
+
+		self.boundary_patches = self.__boundary_patches()
+		self.mesh = self.arterial_tree.get_volume_mesh()
+
 
 
 	#####################################
@@ -32,7 +49,7 @@ class Simulation:
 	#####################################
 
 
-	def boundary_patches(self):
+	def __boundary_patches(self):
 
 		""" Writes the boundary patches as a vtk MultiBlock data. The walls, inlet and oulet surfaces are stored as blocks, with appropriate names
 		"""
@@ -52,27 +69,89 @@ class Simulation:
 
 		crsec_graph = self.arterial_tree.get_crsec_graph()
 
-		for n in crsec_graph.nodes():
-			if crsec_graph.nodes[n]['type'] == "end":
+		for i in range(len(self.inlet_nodes)):
+			n = self.inlet_nodes[i]
+			# Get crsec
+			crsec = crsec_graph.nodes[n]['crsec']
+			center = (crsec[0] + crsec[crsec.shape[0]//2])/2.0
+			vertices, faces = self.arterial_tree.ogrid_pattern(center, crsec)
+			pattern = pv.PolyData(vertices, faces)
 
-				# Get crsec
-				crsec = crsec_graph.nodes[n]['crsec']
-				center = (crsec[0] + crsec[crsec.shape[0]//2])/2.0
-				vertices, faces = self.arterial_tree.ogrid_pattern(center, crsec)
-				pattern = pv.PolyData(vertices, faces)
+			name = "inlet_" + str(i)
+			boundary_blocks[name] = pattern
 
-				if crsec_graph.in_degree(n) == 0: # Inlet case
-					name = "inlet_" + str(inlet_num)
-					inlet_num += 1
-				else:
-					name = "outlet_" + str(outlet_num)
-					outlet_num += 1
+		for i in range(len(self.outlet_nodes)):
+			n = self.outlet_nodes[i]
+			# Get crsec
+			crsec = crsec_graph.nodes[n]['crsec']
+			center = (crsec[0] + crsec[crsec.shape[0]//2])/2.0
+			vertices, faces = self.arterial_tree.ogrid_pattern(center, crsec)
+			pattern = pv.PolyData(vertices, faces)
 
-				boundary_blocks[name] = pattern
+			name = "outlet_" + str(i)
+			boundary_blocks[name] = pattern
 
 		return boundary_blocks
 
 
+	#####################################
+	############# OPENFOAM  #############
+	#####################################
+
+	def file_description(self, ver, fmt, cls, obj):
+		""" Returns the OpenFoam file description 
+
+		Keyword arguments:
+		ver -- version
+		fmt -- format
+		cls -- class
+		obj -- objet 
+		"""
+
+		foamfile = """
+FoamFile
+{
+	version     %.1f;
+	format      %s;
+	class       %s;
+	object      %s;
+}
+""" % (ver, fmt, cls, obj)
+
+		return foamfile
+
+
+	def file_header(self):
+		""" Returns the OpenFoam file header """
+
+		file_header = """/*--------------------------------*- C++ -*----------------------------------*\
+=========                 |
+\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+ \\    /   O peration     | Website:  https://openfoam.org
+  \\  /    A nd           | Version:  8
+   \\/     M anipulation  |
+\*---------------------------------------------------------------------------*/
+"""
+		return file_header
+
+
+	def top_separator(self):
+		""" Returns the OpenFoam top separator """
+
+		top_separator = """
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+"""
+		return top_separator
+
+
+	def bottom_separator(self):
+		""" Returns the OpenFoam top separator """
+
+		bottom_separator = """
+// ************************************************************************* //
+"""
+
+		return bottom_separator
 
 
 	def write_mesh_files(self):
@@ -81,21 +160,12 @@ class Simulation:
 		"""
 
 		output_dir = self.output_dir + "/constant/polyMesh/"
-		volume = self.arterial_tree.get_volume_mesh()
-		surfaces =  self.boundary_patches()
+		volume = self.mesh
+		surfaces =  self.boundary_patches
 
-
-		def write_FoamFile(ver, fmt, cls, obj):
-
-			return """
-		FoamFile
-		{
-			version     %.1f;
-			format      %s;
-			class       %s;
-			object      %s;
-		}
-		""" % (ver, fmt, cls, obj)
+		file_header = self.file_header()
+		top_separator = self.top_separator()
+		bottom_separator = self.bottom_separator()
 
 
 		def get_midpoint(cell):
@@ -114,24 +184,6 @@ class Simulation:
 			return "%d(%s)\n" % (len(face_points), " ".join([str(p) for p in face_points]))
 
 
-
-		file_header = """
-		/*--------------------------------*- C++ -*----------------------------------*\\
-		| =========                 |                                                 |
-		| \\\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
-		|  \\\\    /   O peration     | Version:  2.1.1                                 |
-		|   \\\\  /    A nd           | Web:      www.OpenFOAM.org                      |
-		|    \\\\/     M anipulation  |                                                 |
-		\*---------------------------------------------------------------------------*/
-		"""
-
-		top_separator = """
-		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-		"""
-
-		bottom_separator = """
-		// ************************************************************************* //
-		"""
 		boundary_names = [name for name in surfaces.keys()]
 
 		boundary_mid_points = vtk.vtkPoints()
@@ -211,12 +263,12 @@ class Simulation:
 				internal_faces.append(cell_internal_faces[f])
 
 		for i in range(num_boundaries):
-			print(boundary_names[i] + ":", len(boundary_faces[i]))
+			print(boundary_names[i] + ":", len(boundary_faces[i]) + "faces")
 
 		# write files
 		points_file = open(os.path.join(output_dir, "points"), "w")
 		points_file.write(file_header)
-		points_file.write(write_FoamFile(2.0, "ascii", "vectorField", "points"))
+		points_file.write(self.file_description(2.0, "ascii", "vectorField", "points"))
 		points_file.write(top_separator)
 		num_p = volume.GetNumberOfPoints()
 		pts = volume.GetPoints()
@@ -230,10 +282,9 @@ class Simulation:
 		points_file.close()
 
 
-
 		faces_file = open(os.path.join(output_dir, "faces"), "w")
 		faces_file.write(file_header)
-		faces_file.write(write_FoamFile(2.0, "ascii", "faceList", "faces"))
+		faces_file.write(self.file_description(2.0, "ascii", "faceList", "faces"))
 		faces_file.write(top_separator)
 		total_faces = len(internal_faces)
 		for i in range(num_boundaries):
@@ -250,7 +301,7 @@ class Simulation:
 
 		neighbour_file = open(os.path.join(output_dir, "neighbour"), "w")
 		neighbour_file.write(file_header)
-		neighbour_file.write(write_FoamFile(2.0, "ascii", "labelList", "neighbour"))
+		neighbour_file.write(self.file_description(2.0, "ascii", "labelList", "neighbour"))
 		neighbour_file.write(top_separator)
 		neighbour_file.write("%d\n(\n" % len(internal_faces))
 		for i in range(len(internal_faces)):
@@ -261,7 +312,7 @@ class Simulation:
 
 		owner_file = open(os.path.join(output_dir, "owner"), "w")
 		owner_file.write(file_header)
-		owner_file.write(write_FoamFile(2.0, "ascii", "labelList", "owner"))
+		owner_file.write(self.file_description(2.0, "ascii", "labelList", "owner"))
 		owner_file.write(top_separator)
 		owner_file.write("%d\n(\n" % total_faces)
 		for i in range(len(internal_faces)):
@@ -275,7 +326,7 @@ class Simulation:
 
 		boundary_file = open(os.path.join(output_dir, "boundary"), "w")
 		boundary_file.write(file_header)
-		boundary_file.write(write_FoamFile(2.0, "ascii", "polyBoundaryMesh", "boundary"))
+		boundary_file.write(self.file_description(2.0, "ascii", "polyBoundaryMesh", "boundary"))
 		boundary_file.write(top_separator)
 		start_face = len(internal_faces)
 		boundary_file.write("%d\n(\n" % num_boundaries)
@@ -296,6 +347,126 @@ class Simulation:
 		boundary_file.close()
 		
 
-	def write_boundary_condition_files(self):
+	def write_pressure_boundary_condition_file(self):
 
-		""" Writes the boundary condition files with boundary patches """
+		""" Writes the boundary condition files for pressure """
+
+		boundary_names = [name for name in self.boundary_patches.keys()]
+
+		file = open(self.output_dir + "/0/" + "p", "w")
+		file.write(self.file_header())
+		file.write(self.file_description(2.0, "ascii", "volScalarField", "p"))
+		file.write(self.top_separator())
+
+		file.write("dimensions		[0 2 -2 0 0 0 0];\n\n")
+		file.write("internalField		uniform 0;\n\n")
+		file.write("boundaryField\n{\n\n")
+
+		for i in range(len(boundary_names)):
+			if boundary_names[i] == "wall" or boundary_names[i][:5] == "inlet":
+				tp =  "type		zeroGradient;"
+				value = None
+			else:
+				tp = "type		fixedValue;"
+				value = "value		uniform 0;"
+
+			file.write("\n\t" + boundary_names[i] + "\n\t{\n")
+			file.write("\t\t" + tp + "\n")
+
+			if value is not None:
+				file.write("\t\t" + value + "\n")
+			file.write("\t}\n")
+		file.write("}\n")
+
+		file.write(self.bottom_separator())
+		file.close()
+
+
+	def write_velocity_boundary_condition_file(self, vel_mag):
+
+		""" Writes the boundary condition files for velocity 
+
+		Keyword argument:
+		vel_mag -- list of inlet velocity magnitude in m/s
+		"""
+
+		boundary_names = [name for name in self.boundary_patches.keys()]
+
+		file = open(self.output_dir + "/0/" + "U", "w")
+		file.write(self.file_header())
+		file.write(self.file_description(2.0, "ascii", "volVectorField", "U"))
+		file.write(self.top_separator())
+
+		file.write("dimensions		[0 1 -1 0 0 0 0];\n\n")
+		file.write("internalField		uniform (0 0 0);\n\n")
+		file.write("boundaryField\n{\n\n")
+
+		U_inlet = []
+		spl_graph = self.arterial_tree.get_spline_graph()
+
+		for i in range(len(self.inlet_nodes)):
+			succ = list(spl_graph.successors(self.inlet_nodes[i]))[0]
+			U_inlet.append(spl_graph.edges[(self.inlet_nodes[i], succ)]['spline'].tangent(0.0) * vel_mag[i])
+
+		for i in range(len(boundary_names)):
+			if boundary_names[i] == "wall":
+				tp =  "type		noSlip;"
+				value = None
+			elif boundary_names[i][:5] == "inlet":
+				num = int(boundary_names[i][-1])
+				tp =  "type		fixedValue;"
+				value = "value		uniform (" + str(U_inlet[num][0]) + " " + str(U_inlet[num][1]) + " " + str(U_inlet[num][2])+");"
+			else:
+				tp = "type		zeroGradient;"
+				value = None
+
+			file.write("\n\t" + boundary_names[i] + "\n\t{\n")
+			file.write("\t\t" + tp + "\n")
+
+			if value is not None:
+				file.write("\t\t" + value + "\n")
+			file.write("\t}\n")
+		file.write("}\n")
+
+		file.write(self.bottom_separator())
+		file.close()
+
+
+
+	#####################################
+	########## VISUALIZATION  ###########
+	#####################################
+
+	def import_field(self, filename, fieldname, outfieldname):
+		""" Imports a scalar or vector field from the simulation result as a vtk multiblock file and add it to the volume mesh
+
+		Keyword arguments:
+		filename -- name of the vtk multiblock file
+		fieldname -- name of the cell array with the field of interest"""
+
+		res = pv.read(filename)
+		mesh = res.get(0).get(0)
+		field = mesh.cell_arrays([fieldname])
+		self.mesh[outfieldname] = field
+
+	
+
+	def display_crsec(self, step, fieldname):
+		
+		""" Displays cross sections along the vessels 
+
+		Keyword arguments:
+		step -- step size for slicing
+		fieldname -- field to display"""
+
+		#slc = self.mesh.slice(normal=, origin=)
+
+		p = pv.Plotter()
+		p.add_mesh(slc) 
+		p.show() 
+
+
+
+
+
+
